18cef058bb9017a88149c40d74fc09758c939a94
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-05-27 13:35:01 +0300

    Add new challange 23

--- script.js
+++ script.js
@@ -66,3 +66,28 @@ function areThereDuplicates2(...args) {
 
 // console.log(areThereDuplicates2(1, 2, 2)); //true
 // console.log(areThereDuplicates2(1, 2, 3)); //false
+
+///////////////////////////////////////////////
+// 23. averagePair
+/* 
+Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+Bonus Constraints // Time - O(n)  ; Space - O(1)
+*/
+
+function averagePair(arr, num) {
+    let sum,
+        targetsum = 2 * num;
+    let i = 0,
+        j = arr.length - 1;
+
+    while (j > i) {
+        sum = arr[i] + arr[j];
+        // console.log(sum);
+        if (targetsum === sum) return true;
+        else if (targetsum < sum) j--;
+        else i++;
+    }
+    return false;
+}
+
+// console.log(averagePair([1, 2, 3], 2.5)); //true
