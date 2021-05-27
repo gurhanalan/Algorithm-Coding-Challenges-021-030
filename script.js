321997b7e25b3a286a3bf8960a16f39967ad7946
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-05-27 11:04:42 +0300

    Add new challange 22

--- script.js
+++ script.js
@@ -27,3 +27,42 @@ function sameFrequency(num1, num2) {
 }
 
 // console.log(sameFrequency(18245622, 28165422));
+
+// ////////////////////////////////
+// 22. areThereDuplicates
+/*
+Implement a function called, areThereDuplicates  which accepts a variable number of arguments,  and checks whether there are any duplicates among the arguments passed in. You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+Restrictions: 
+Time - O(n) 
+Space - O(n) 
+Bonus:
+Time - O(n logn) 
+Space - O(1) 
+*/
+
+// Time - O(n)  ; Space - O(n)
+
+function areThereDuplicates(...args) {
+    const lookup = {};
+
+    for (let char of args) {
+        if (lookup[char]) return true;
+        lookup[char] ? (lookup[char] += 1) : (lookup[char] = 1);
+    }
+
+    return false;
+}
+
+// Time - O(n 2) - Space - O(1)
+function areThereDuplicates2(...args) {
+    for (let i = 0; i < args.length; i++) {
+        for (let j = i + 1; j < args.length; j++) {
+            if (args[i] === args[j]) return true;
+        }
+    }
+
+    return false;
+}
+
+// console.log(areThereDuplicates2(1, 2, 2)); //true
+// console.log(areThereDuplicates2(1, 2, 3)); //false
