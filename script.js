dff4da2498edcbc5f1e3b8809c2b1a7acb8dddd4
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-05-27 15:19:34 +0300

    Add new challange 24

--- script.js
+++ script.js
@@ -91,3 +91,30 @@ function averagePair(arr, num) {
 }
 
 // console.log(averagePair([1, 2, 3], 2.5)); //true
+
+////////////////////////////////
+// 24. isSubsequence
+/* 
+Write a function called  isSubsequence  which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+Your solution MUST have AT LEAST the following complexities: 
+Time Complexity - O(N + M)
+Space Complexity - O(1)
+*/
+
+function isSubsequence(str1, str2) {
+    let i = 0,
+        j = 0;
+
+    while (j < str2.length) {
+        if (str1[i] === str2[j]) {
+            i++;
+            j++;
+            if (i === str1.length) return true;
+        } else j++;
+    }
+    return false;
+}
+
+// console.log(isSubsequence("sing", "sting")); //true
+// console.log(isSubsequence("abc", "acb")); //false
